186e82dffc4f8d7c4851ca3a5807d575c01a5594
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-10-23 16:08:05 +0200

    Refactor code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # IDB-promise-wrapper
 
 ## Minimal promise wrapper for IDB connection.
+- Allows to get and save data from single onject stored in IDB.
--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-
-/**
- * Opens database connection.
- * https://developer.mozilla.org/en-US/docs/Web/API/IDBOpenDBRequest
- */
-function openUserDB () {
-  return new Promise((resolve, reject) => {
-    let openRequest = indexedDB.open('app_data', 1);
-    openRequest.onsuccess = () => resolve(openRequest.result);
-    openRequest.onupgradeneeded = () => {
-      let db =  openRequest.result;
-      db.onerror = reject;
-      db.createObjectStore('app_data', { autoIncrement: true });
-      db.onversionchange = resolve;
-    };
-    openRequest.onblocked = reject;
-    openRequest.onerror = reject;
-  });
-}
-
 /**
  * Get data from app data store.
  * https://developer.mozilla.org/en-US/docs/Web/API/IDBTransaction
  * https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore
  * @param {string} key - optional key to get from object
+ * @returns {*}
  */
-function getAppDataFromDB (key) {
+
+function getFromDB (key) {
   return openUserDB().then(db => new Promise((resolve, reject) => {
     let transaction = db.transaction(['app_data'], 'readwrite');
     let dataRequest = transaction.objectStore('app_data').get(1);
@@ -42,9 +24,10 @@ function getAppDataFromDB (key) {
  * https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore
  * @param {string} key - key to save
  * @param {*} value - value to save
+ * @returns {*}
  */
-function saveAppDataToDB (key, value) {
-  return openUserDB().then().then(db => new Promise((resolve, reject) => {
+function saveToDB (key, value) {
+  return new Promise((resolve, reject) => {
     return getAppDataFromDB().then(dataObject => {
       if (dataObject) {
         dataObject[key] = value;
@@ -53,7 +36,7 @@ function saveAppDataToDB (key, value) {
           [key]: value,
         };
       }
-      let transaction = db.transaction(['app_data'], 'readwrite');
+      let transaction = this.database.transaction(['app_data'], 'readwrite');
       let dataRequest = transaction.objectStore('app_data').put(dataObject, 1);
       transaction.oncomplete = resolve(new Promise((resolve, reject) => {
         dataRequest.onerror = reject;
@@ -61,5 +44,40 @@ function saveAppDataToDB (key, value) {
       }));
       transaction.onerror = reject;
     });
-  }));
+  });
+}
+
+/**
+ * Creates Database Manager
+ * @class
+ * @classdesc Allows to perform basic operations on database.
+ * @property {IDBDatabase} database
+ * @property {saveToDB} saveToDB
+ * @property {getFromDB} getFromDB
+ * @param {IDBDatabase} database
+ */
+function DBManager (database) {
+  this.database = database;
+  this.saveToDB = saveToDB;
+  this.getFromDB = getFromDB;
+}
+
+/**
+ * Opens database connection.
+ * https://developer.mozilla.org/en-US/docs/Web/API/IDBOpenDBRequest
+ * @returns {DBManager}
+ */
+module.exports = function openDB (dataBaseName, storeName) {
+  return new Promise((resolve, reject) => {
+    let openRequest = indexedDB.open(dataBaseName, 1);
+    openRequest.onsuccess = () => resolve(new DBManager(openRequest.result));
+    openRequest.onupgradeneeded = () => {
+      let db =  openRequest.result;
+      db.onerror = reject;
+      db.createObjectStore(storeName, { autoIncrement: true });
+      db.onversionchange = resolve;
+    };
+    openRequest.onblocked = reject;
+    openRequest.onerror = reject;
+  });
 }
